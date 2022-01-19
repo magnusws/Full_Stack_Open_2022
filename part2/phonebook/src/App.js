c13c3131813b488d3760bc5e9c06418d1396b2b6
Magnus Walberg Solbakken <magnusws@stud.ntnu.no>
2022-01-19 16:13:16 +0100

    phonebook part 1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,56 @@
 import React, { useState } from 'react'
 
+const Person = ({name}) => {
+  return (
+    <tr>
+      <td>
+        {name}
+      </td>
+    </tr>)
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ])
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name:  
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <table>
+        <tbody>
+          {persons.map(person =>
+            <Person key={person.name} name={person.name} />
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }
