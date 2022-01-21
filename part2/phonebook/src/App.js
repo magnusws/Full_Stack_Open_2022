a9779924d72d943639c88e370edfb8276d13d415
Magnus Walberg Solbakken <magnusws@stud.ntnu.no>
2022-01-21 15:06:04 +0100

    phonebook part 2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,7 @@
 import React, { useState } from 'react'
-
-const Person = ({name, number}) => {
-  return (
-    <tr>
-      <td>
-        {name} {number}
-      </td>
-    </tr>)
-}
-
-// Check if the key and values og two person objects 
-const areThesePersonsEqual = (first, second) => {
-  if (first.name === second.name) return true
-  if (first.number === second.number) return true
-  return false
-}
-// this.state.projects.filter(val => 
-// val.title.toLowerCase().indexOf(search.toLowerCase()) > -1
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -28,41 +13,7 @@ const App = () => {
 
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
-  const [newSearch, setNewSearch] = useState('')
-  const [personsToShow, setPersonsToShow] = useState(persons)
-
-  const addName = (event) => {
-    var validInput = false
-    var doesAlreadyExist = false
-
-    event.preventDefault()
-
-    if(newName !== '' && newPhoneNumber !== '') {
-      validInput = true
-    }
-
-    if(validInput){
-
-      const personObject = {
-        name: newName,
-        number: newPhoneNumber,
-        id: persons.length + 1
-      }
-
-      persons.forEach(person => {
-        doesAlreadyExist = areThesePersonsEqual(person, personObject)
-      })
-
-      if (!doesAlreadyExist) {
-        setPersons(persons.concat(personObject))
-        setNewName('')
-        setNewPhoneNumber('')
-
-      }else {
-        alert(`${newName} is already added to phonebook`)
-      }
-    }
-  }
+  const [search, setSearch] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -73,52 +24,57 @@ const App = () => {
   }
 
   const handleSearchChange = (event) => {
-    setNewSearch(event.target.value)
-    // if(newSearch === '') setPersonsToShow(persons)
-    setPersonsToShow(persons.filter(person =>
-      person.name.toLowerCase().indexOf(newSearch.toLowerCase()) > -1))
+    setSearch(event.target.value)
   }
 
+  const addPerson = (event) => {
+    var validInput = false
+    var doesAlreadyExist = false
+    
+    event.preventDefault()
+
+    validInput = newName !== '' && newPhoneNumber !== '' // input in form fields?
+      ? true 
+      : false
+
+    if (validInput) {                    // if both fields in the form has text
+      
+      doesAlreadyExist = persons         // checks if person already exist
+        .map(person => person.name)
+        .includes(newName)
+      
+      if (!doesAlreadyExist) {            // if a person does not already exist
+
+        const personObject = {            // new person obj
+          name: newName,
+          number: newPhoneNumber,
+          id: persons.length + 1
+        }
+
+        setPersons(persons.concat(personObject))  // add new person obj
+        setNewName('')                           
+        setNewPhoneNumber('')
+        
+      } else {                                    // alert if person alredy exist
+        alert(`${newName} is already added to phonebook`)
+      }
+    }
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <p>filter shown with</p>
-      <input
-        value={newSearch}
-        onChange={handleSearchChange}
-      />
+      <Filter onChange={handleSearchChange} value={search}/>
       <h2>Add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name:  
-          <input 
-            value={newName}
-            onChange={handleNameChange}
-          />
-          <br></br>
-          number:
-          <input
-            value={newPhoneNumber}
-            onChange={handlePhoneNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm 
+        onSubmit={addPerson}
+        newName={newName} 
+        handleNameChange={handleNameChange}
+        newPhoneNumber={newPhoneNumber}
+        handlePhoneNumberChange={handlePhoneNumberChange}
+      />
       <h2>Numbers</h2>
-      <table>
-        <tbody>
-          {personsToShow.map(person =>
-            <Person 
-              key={person.id} 
-              name={person.name} 
-              number={person.number}
-            />
-          )}
-        </tbody>
-      </table>
+      <Persons persons={persons} search={search}/>
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Person from './Person'
+
+
+const Persons = ({search, persons}) => {
+  return (
+    <table>
+      <tbody>
+        {filterPersons(search, persons).map(person =>
+          <Person
+            key={person.id}
+            name={person.name}
+            number={person.number}
+          />
+        )}
+      </tbody>
+    </table>
+  )
+}
+
+// Default(no input): returns an array with every single person.
+// If search input: returns an array with matching persons if any.
+const filterPersons = (search, persons) => {
+  if (search !== '') {
+    return (persons.filter(person =>
+      person.name.toLowerCase().indexOf(search.toLowerCase()) > -1))
+  } else {
+    return persons
+  }
+}
+
+export default Persons
